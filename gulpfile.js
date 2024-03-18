8000c6d0bb292597619d4309db3bcefc920ba40a
Antonioog <arkanamos1985@gmail.com>
2024-03-18 00:23:55 -0600

    Se prepara el entorno de desarrollo para iniciar con el proyecto.

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,27 @@
-const { src, dest, watch } = require("gulp");
+const { src, dest, watch, series, parallel } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
 
-function css( done ) {
+function css(done) {
   //compilar sass
   // pasos: 1- Identificar archivo, 2- Compilarla, 3- Guardar el .css
 
   //Identificamos el archivo.
   src("src/scss/app.scss")
-    .pipe( sass() )
-    .pipe( postcss( [ autoprefixer ] ) )
-    .pipe( dest("build/css") )
+    .pipe(sass())
+    .pipe(postcss([autoprefixer]))
+    .pipe(dest("build/css"));
   done();
 }
 
 function dev() {
-  watch( "src/scss/app.scss", css );
+  watch("src/scss/**/*.scss", css);
 }
+
 exports.css = css;
 exports.dev = dev;
+exports.default = series(css, dev);
+
+//Series - Se inicia una tarea, y hasta que finaliza, inicia la siguiente.
+//Parallel - Todas las tareas inician al mismo tiempo
