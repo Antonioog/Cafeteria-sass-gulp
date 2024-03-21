726b394cbef917904288d4442e877afc0115b0bf
Antonioog <arkanamos1985@gmail.com>
2024-03-20 22:45:51 -0600

    Terminamos el header en version desktop

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
+//Dependencias de CSS Y SASS
 const { src, dest, watch, series, parallel } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
+
+//Dependencias de Imagenes
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const avif = require("gulp-avif");
 
 function css(done) {
   //compilar sass
@@ -15,13 +21,41 @@ function css(done) {
   done();
 }
 
+function imagenes() {
+  return src("src/img/**/*")
+    .pipe(imagemin({ optimizationLevel: 3 }))
+    .pipe(dest("build/img"));
+}
+
+function versionWebp() {
+  const opciones = {
+    quality: 50,
+  };
+  return src("src/img/**/*.{jpg,png}")
+    .pipe(webp(opciones))
+    .pipe(dest("build/img"));
+}
+
+function versionAvif() {
+  const opciones = {
+    quality: 50,
+  };
+  return src("src/img/**/*.{jpg,png}")
+    .pipe(avif(opciones))
+    .pipe(dest("build/img"));
+}
+
 function dev() {
   watch("src/scss/**/*.scss", css);
+  watch("src/img/**/*", imagenes);
 }
 
 exports.css = css;
 exports.dev = dev;
-exports.default = series(css, dev);
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.default = series(imagenes, versionWebp, versionAvif, css, dev);
 
 //Series - Se inicia una tarea, y hasta que finaliza, inicia la siguiente.
 //Parallel - Todas las tareas inician al mismo tiempo
